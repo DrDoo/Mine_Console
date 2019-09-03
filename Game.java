f29121c3ee6557ae91da70e1f690054f56c741ce
Shayaan <shayaansindhoo01@gmail.com>
2019-09-03 20:25:11 +0100

    Player Turns and Game Logic
    Player can now actually play the game and game logic such as win loss status are implemented, tho can not be changed as of yet. Additionally grid sizing now uses variables that eventually will be able to be player determined

--- Game.java
+++ Game.java
@@ -1,31 +1,56 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
 
+  // The grid the game is played on and two variables used to determine its size
   static Cell[][] grid;
+  static int gridX;
+  static int gridY;
+
+  // Booleans used to determine the win/loss state of the game
+  static boolean gameWon = false;
+  static boolean gameLost = false;
+
+  // Tools to help with various parts of the game
   static Random random = new Random();
+  static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
 
-    grid = new Cell[16][10];
+    // Currently grid size is hardcoded
+    gridX = 16;
+    gridY= 10;
+    grid = new Cell[gridX][gridY];
 
+    // Initialisation of the game
+    initialise();
+
+    // Whist the player has neither won or lost the game they can continue to
+    // make movses
+    while (!gameWon && !gameLost) {
+      playerTurn();
+    }
+
+  }
+
+  // Initialises the game
+  public static void initialise() {
     // Initialising the game. Cells are created and mines placed randomly
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 16; j++) {
+    for (int i = 0; i < gridY; i++) {
+      for (int j = 0; j < gridX; j++) {
         grid[j][i] = new Cell();
         if (random.nextInt(100) < 20)
           grid[j][i].setMine();
       }
     }
 
-
-
     // Finding out the number of bordering mines for each cell
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 16; j++) {
+    for (int i = 0; i < gridY; i++) {
+      for (int j = 0; j < gridX; j++) {
         int borderMines = 0;
-        for (int m = 0; m < 10; m++) {
-          for (int n = 0; n < 16; n++) {
+        for (int m = 0; m < gridY; m++) {
+          for (int n = 0; n < gridX; n++) {
             if (!(m==i && n==j)) {
               if (isMine(j, i, n, m))
                 borderMines++;
@@ -53,14 +78,52 @@ public class Game {
   // Prints out the visual of the game
   public static void printGrid() {
 
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 16; j++) {
+    for (int i = 0; i < gridY; i++) {
+      for (int j = 0; j < gridX; j++) {
         System.out.print("|");
         System.out.print(grid[j][i]);
       }
       System.out.print("|");
       System.out.println();
     }
+  }
+
+  // Takes the players input and does the corresponding action
+  public static void playerTurn() {
+    System.out.println("Choose Action, type help for a list of commands");
+    // Stores the players input which is then compared to the possible inputs
+    String playerAction = scanner.next();
+
+    // Variables to help with choosing the cell the action is happening to
+    String chosenCell;
+    int chosenX;
+    int chosenY;
+
+    // If they choose help they are told the possible actions
+    if (playerAction.equals("help"))
+      System.out.println("Possible actions are reveal and flag");
+
+    // If they choose flag they then pick the cell, which is then flagged
+    else if (playerAction.equals("flag")) {
+      System.out.println("Choose which cell in the format x,y");
+      chosenCell = scanner.next();
+      chosenX = Integer.parseInt(chosenCell.substring(0, 1));
+      chosenY = Integer.parseInt(chosenCell.substring(2));
+      grid[chosenX][chosenY].flag();
+      printGrid();
+    }
+    // If they choose reveal they then pick the cell, which is then revealed
+    else if (playerAction.equals("reveal")) {
+      System.out.println("Choose which cell in the format x,y");
+      chosenCell = scanner.next();
+      chosenX = Integer.parseInt(chosenCell.substring(0, 1));
+      chosenY = Integer.parseInt(chosenCell.substring(2));
+      grid[chosenX][chosenY].reveal();
+      printGrid();
+    }
+    // If they put in some other input it is ignored
+    else
+      System.out.println("Invalid action");
   }
 
 }
