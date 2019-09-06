673ddcd73750ea31577b9a3d9ac81581e3fdd987
Shayaan <shayaansindhoo01@gmail.com>
2019-09-06 18:01:00 +0100

    Text Colouring
    Cells now have their number of bordering mines coloured to help differentiate them

--- Cell.java
+++ Cell.java
@@ -3,8 +3,21 @@ public class Cell {
   // Variables the hold the state of the Cell
   private boolean isMine;
   private int borderingMines;
+  private String textColour;
   private boolean revealed;
   private boolean flagged;
+
+  // These are the colours used to change the text colour in the terminal
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_BLACK = "\u001B[30m";
+  public static final String ANSI_BLUE = "\u001B[34m";
+  public static final String ANSI_GREEN = "\u001B[32m";
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_PURPLE = "\u001B[35m";
+  // Black
+  public static final String ANSI_ORANGE = "\u001b[31;1m"; // Actually Bright Red
+  public static final String ANSI_GRAY = "\\u001b[37m"; // Actually White
+  public static final String ANSI_CYAN = "\u001B[36m";
 
   // Each cell is initialised as not being revealed or flagged and is not a mine
   public Cell() {
@@ -16,6 +29,35 @@ public class Cell {
   // Method to set the number of bordering mines the cell has
   public void setBorderMines(int numberOfBorderMines) {
     borderingMines = numberOfBorderMines;
+    switch(numberOfBorderMines) {
+      case 0:
+        textColour = ANSI_RESET;
+        break;
+      case 1:
+        textColour = ANSI_BLUE;
+        break;
+      case 2:
+        textColour = ANSI_GREEN;
+        break;
+      case 3:
+        textColour = ANSI_RED;
+        break;
+      case 4:
+        textColour = ANSI_PURPLE;
+        break;
+      case 5:
+        textColour = ANSI_BLACK;
+        break;
+      case 6:
+        textColour = ANSI_ORANGE;
+        break;
+      case 7:
+        textColour = ANSI_GRAY;
+        break;
+      case 8:
+        textColour = ANSI_CYAN;
+        break;
+    }
   }
 
   // Method to change a cell into a mine
@@ -41,10 +83,13 @@ public class Cell {
 
   // The toString method is used to display the cell on the console
   public String toString() {
-    if (revealed && !isMine)
-      return ("" + borderingMines);
-      if (revealed && isMine)
-        return "*";
+    if (revealed && !isMine){
+      if (borderingMines == 0)
+        return " ";
+      return (textColour + borderingMines + ANSI_RESET);
+    }
+    if (revealed && isMine)
+      return "*";
     if (flagged)
       return "F";
     return "#";
@@ -54,7 +99,7 @@ public class Cell {
   public boolean getMineStatus() {
     return isMine;
   }
-  
+
   public int getBorderingMines() {
     return borderingMines;
   }
